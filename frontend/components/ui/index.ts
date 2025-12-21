a6c867111e06ebdbb26da811ee0b585337a20376
ParrvLuthra22 <parrvluthra@gmail.com>
2025-12-21 18:45:28 +0530

    Add UI components index for proper exports

---
+++ frontend/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as Container } from './Container';
+export { default as Section } from './Section';
+export { default as GlassCard } from './GlassCard';
+export { Input } from './input';
+export { Label } from './label';
